9935c2f4d26f9b03ec46433066df11c38506ed55
DonovanDonas <donovan.barcena.7@gmail.com>
2020-01-08 16:57:08 -0600

    FullScreen Informacion and Juego

--- app/src/main/java/com/example/david/sec/MainActivity.java
+++ app/src/main/java/com/example/david/sec/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.annotation.DrawableRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -48,6 +50,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         //Poner icono en action Bar
